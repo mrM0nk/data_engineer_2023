a12732132c3f51687a940f2ffe7851541eadcbd2
Maksym <Maksym199013@gmail.com>
2023-04-07 19:28:25 +0300

    mid commit of implementation filtering args

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ def get_args(argv: Optional[Sequence[str]] = None):
     developer: Maksym Sukhorukov"""
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-year_from', type=int, default=None, help='')
-    parser.add_argument('-year_to', type=int, default=None, help='')
-    parser.add_argument('-brand', type=str, default=None, help='')
-    parser.add_argument('-model', type=str, default=None, help='')
-    parser.add_argument('-price_from', type=int, default=None, help='')
-    parser.add_argument('-price_to', type=int, default=None, help='')
-    parser.add_argument('-transmission', type=str, default=None, help='')
-    parser.add_argument('-mileage', type=int, default=None, help='')
+    parser.add_argument('-year_from', type=int, default=None, help='only digit values')             # implemented
+    parser.add_argument('-year_to', type=int, default=None, help='only digit values')               # implemented
+    parser.add_argument('-brand', type=str, default=None, help='')                                  # implemented
+    parser.add_argument('-model', type=str, default=None, help='')                                  # implemented
+    parser.add_argument('-price_from', type=int, default=None, help='supported only USD values')    # implemented
+    parser.add_argument('-price_to', type=int, default=None, help='supported only USD values')      # implemented
+    parser.add_argument('-transmission', type=str, default=None, help='')                           # implemented
+    parser.add_argument('-mileage', type=int, default=None, help='')                                # implemented
     parser.add_argument('-body', type=str, default=None, help='')
     parser.add_argument('-engine_from', type=int, default=None, help='')
     parser.add_argument('-engine_to', type=int, default=None, help='')
@@ -35,12 +35,15 @@ def get_args(argv: Optional[Sequence[str]] = None):
 params = get_args()
 
 
+
+
+
 def parse_file(params):
 
     header_line = True
     counter = 0                         # for testing
 
-    with open(params.source_file, 'r', encoding='utf-8') as file:  # , encoding='utf-8'
+    with open(params.source_file, 'r', encoding='utf-8') as file:  # encoding='utf-8'
         cars = csv.reader(file)
         for row in cars:
             if header_line:
@@ -56,11 +59,26 @@ def parse_file(params):
             title = row[1]
             try:
                 title_deal_type = re.split(r'^(\S+)\s+(\S+)\s+([^·]+)((\·)\s+(.+))?$|$', title.split(',')[0])[1].strip()
+            except:
+                title_deal_type = None
+            try:
                 title_brand = re.split(r'^(\S+)\s+(\S+)\s+([^·]+)((\·)\s+(.+))?$|$', title.split(',')[0])[2].strip()
+                if params.brand and params.brand.upper() not in title_brand.upper():
+                    continue
+            except:
+                title_brand = None
+            try:
                 title_model = re.split(r'^(\S+)\s+(\S+)\s+([^·]+)((\·)\s+(.+))?$|$', title.split(',')[0])[3].strip()
+                if params.model and params.model.upper() not in title_model.upper():
+                    continue
+            except:
+                title_model = None
+
+            try:
                 title_restyling = re.split(r'^(\S+)\s+(\S+)\s+([^·]+)((\·)\s+(.+))?$|$', title.split(',')[0])[6].strip()
             except:
-                title_deal_type, title_brand, title_model, title_restyling = None, None, None, None
+                title_restyling = None
+
 
             price_primary = row[2]
             try:
@@ -71,6 +89,9 @@ def parse_file(params):
             price_secondary = row[3]
             try:
                 price_secondary_processed = (int((''.join(x for x in row[3] if x.isdigit()))), 'usd')
+                if (params.price_from and price_secondary_processed[0] < int(params.price_from)) or \
+                        (params.price_to and price_secondary_processed[0] > int(params.price_to)):
+                    continue
             except:
                 price_secondary_processed = (None, 'usd')
 
@@ -97,12 +118,19 @@ def parse_file(params):
             description = row[7]
             try:
                 description_year = int(''.join(x for x in description.split('|')[0].split(',')[0] if x.isdigit()))
+                if (params.year_from and description_year < int(params.year_from)) or \
+                        (params.year_to and description_year > int(params.year_to)):
+                    continue
             except:
                 description_year = None
+
             try:
                 description_transmission = description.split('|')[0].split(',')[1].strip()
+                if params.transmission and params.transmission.upper() not in description_transmission.upper():
+                    continue
             except:
                 description_transmission = None
+
             try:
                 description_engine = int(''.join(x for x in description.split('|')[0].split(',')[2]if x.isdigit()) +
                                          '00')
@@ -112,14 +140,17 @@ def parse_file(params):
                 if description_engine == 0:
                     description_fuel = description.split('|')[0].split(',')[2].strip()
                 else:
-                    description_fuel = description.split('|')[0].split(',')[3].strip()
+                    description_fuel = description.split('|')[0].split(',')[-2].strip()
             except:
                 description_fuel = None
             try:
-                description_mileage = (int(''.join(x for x in description.split('|')[0].split(',')[4] if x.isdigit())),
+                description_mileage = (int(''.join(x for x in description.split('|')[0].split(',')[-1] if x.isdigit())),
                                        'km')
+                if params.mileage and description_mileage[0] > int(params.mileage):
+                    continue
             except:
                 description_mileage = None
+
             try:
                 description_body = description.split('|')[1].split(',')[0].strip()
             except:
@@ -141,7 +172,7 @@ def parse_file(params):
                         comment + ' ' + description + ' ' + exchange + ' ' + scrap_date
 
 
-            print(full_card)
+            #print(full_card)
 
 
 
